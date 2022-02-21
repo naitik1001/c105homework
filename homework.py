1532dc2c96f7837e8a0af0870f4b0fec453920b6
Naitik Jindal <91480898+naitik1001@users.noreply.github.com>
2022-02-21 19:05:32 +0530

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,24 @@
+import math
+import csv
+with open("C:/Users/gogof/Desktop/coding/hi-main/c105/homework.csv",newline = '' ) as f:
+    reader =  csv.reader(f)
+    file_data = list(reader)
+data = file_data[0]
+def mean(data):
+    n = len(data)
+    total = 0
+    for x in data:
+        total +=int(x)
+        mean =  total/n
+        return mean
+squared_list = []
+for  number in data:
+    a = int(number)-mean(data)
+    a = a**2        
+    squared_list.append(a)
+sum = 0
+for i in squared_list:
+    sum = sum + i
+result = sum/(len(data)-1)
+standard_deviation =  math.sqrt(result)
+print(standard_deviation)
